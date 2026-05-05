cd5597f79500aca6caad03d8432985d187dd64a2
MikshunasAN <MikshunasAN@it.mos.ru>
2026-05-05 08:02:18 +0300

    modify task_1.py, task_2.py: add comments, delete commented-out code

--- task_1.py
+++ task_1.py
@@ -1,5 +1,3 @@
-# задание ГОТОВО
-
 class Case:
     def __init__(self, test_case_id, name, step_description, expected_result):
         self.test_case_id = test_case_id
@@ -13,13 +11,13 @@ class Case:
               f"\nОписание шага: {self.step_description}"
               f"\nОжидаемый результат: {self.expected_result}")
 
-class ExtendedCase(Case):
-    def __init__(self, test_case_id, name, step_description, expected_result, precondition: int, enviroment: int):
-        super().__init__(test_case_id, name, step_description, expected_result)
-        self.precondition = precondition #add type string
-        self.enviroment = enviroment #add type string
+class ExtendedCase(Case): # создал подкласс и добавил ему два новых атрибута с типом данных строка
+    def __init__(self, test_case_id, name, step_description, expected_result, precondition: str, enviroment: str):
+        super().__init__(test_case_id, name, step_description, expected_result) # вызвал конструктор через super()
+        self.precondition = precondition
+        self.enviroment = enviroment
 
-    def print_test_case_info(self):
+    def print_test_case_info(self): # переопределил метод: добавил печать новых атрибутов
         print(f"ID тест-кейса:  {self.test_case_id}"
               f"\nНазвание: {self.name}"
               f"\nОписание шага: {self.step_description}"
@@ -30,4 +28,4 @@ class ExtendedCase(Case):
 
 case = ExtendedCase('1', 'Наличие кнопки Принять', '1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ', 'Кнопка доступна', 'Открыть сервис', 'Яндекс Браузер')
 
-case.print_test_case_info()
+case.print_test_case_info() # создал объект case и вызвал для него метод печати
--- task_2.py
+++ task_2.py
@@ -1,38 +1,21 @@
-# задание ГОТОВО
-# убрать закомментированное и текст задания, перепроверить
-# добавить корректные комментарии 
-
-# # Создай класс Movies:
-# проинициализируй в нём пустой список self.movies через конструктор;
-# добавь метод add_movie(). Он будет принимать параметр movie и добавлять его в конец списка self.movies.
-# Создай два дочерних класса — Comedy и Drama. Они наследуют метод add_movie(). Метод этих классов должен принимать параметр movie и добавлять его в конец списка self.movies. Затем возвращать записи вида Комедии: '[]' и Драмы: '[]' соответственно.
-# Вызови метод add_movie() для объекта Comedy(). Входной параметр — 'Большой куш'. Выведи на экран результат.
-# Вызови метод add_movie() для объекта Drama(). Входной параметр — 'Оружейный барон'. Выведи на экран результат.
-
-# Чтобы добавить элемент в конец списка, нужен метод append(). То есть так: self.movies.append(movie).
-# Чтобы вернуть значение, используй return. Понадобится вернуть значение в определённом виде: return f'Комедии: {self.movies}'.
-
 class Movies: 
 
     def __init__(self):
         self.movies = [] # инициализировал пустой список в суперклассе через конструктор
     
-    def add_movie(self, movie):
-        self.movies.append(movie) # добавил метод add_movie с добавлением параметра в конец списка
+    def add_movie(self, movie): # добавил метод add_movie: добавляет фильм в конец списка
+        self.movies.append(movie) 
         return self.movies
     
 class Comedy(Movies):
 
-#    def __init__(self):
-#        super().__init__() # ...
-
-    def add_movie(self, movie):
-        self.movies = super().add_movie(movie) # ...
-        return f'Комедии: {self.movies}' # ... вернул значение в виде f-строки со значениями списка self.movies внутри
+    def add_movie(self, movie): # переопределил метод для подкласса: возвращает список комедий в виде f-строки
+        self.movies = super().add_movie(movie) 
+        return f'Комедии: {self.movies}' 
 
 class Drama(Movies):
 
-    def add_movie(self, movie):
+    def add_movie(self, movie): # переопределил метод для подкласса: возвращает список драм в виде f-строки
         self.movies.append(movie)
         return f'Драмы: {self.movies}'
 
